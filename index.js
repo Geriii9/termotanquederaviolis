901d51fe145ed3696a341a6861789af8f741ed34
Articunogem12 <adrifer1705@gmail.com>
2022-10-10 20:56:56 +0200

    copiar palabras y retoques

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
-const span = document.querySelector("span");
+// const span = document.querySelector("span");
 
-span.onclick = function() {
-  document.execCommand("copy");
+// span.onclick = function () {
+//     document.execCommand("copy");
+// }
+
+// span.addEventListener("copy", function (event) {
+//     event.preventDefault();
+//     if (event.clipboardData) {
+//         event.clipboardData.setData("text/plain", span.textContent);
+//         console.log(event.clipboardData.getData("sudo apt install apache2"))
+//     }
+// });
+
+
+function copy(that) {
+    var inp = document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value = that.textContent
+    inp.select();
+    document.execCommand('copy', false);
+    inp.remove();
 }
-
-span.addEventListener("copy", function(event) {
-  event.preventDefault();
-  if (event.clipboardData) {
-    event.clipboardData.setData("text/plain", span.textContent);
-    console.log(event.clipboardData.getData("sudo apt install apache2"))
-  }
-});
